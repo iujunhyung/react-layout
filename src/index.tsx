65a0ff5d5b9512481b474d091f51e0fa79173dc2
junehyung <jhcj910805@gmail.com>
2024-01-30 22:46:52 +0900

    Update ReactLayout component to display updated time

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ export function ReactLayout({ children }: any) {
   return (
     <div className={styles.root}>
       {children}
-      <span>{time} dd</span>
+      <span>{time} aaadd</span>
     </div>
   );
 }
