c634c36f14a5716ab25075d8f3acf47cc6104672
junehyung <jhcj910805@gmail.com>
2024-01-30 23:31:44 +0900

    Update ReactLayout component to display additional text

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ export function ReactLayout({ children }: any) {
   return (
     <div className={styles.root}>
       {children}
-      <span>{time} aaadd</span>
+      <span>{time} aaadd dd</span>
     </div>
   );
 }
