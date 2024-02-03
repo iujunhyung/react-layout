881ee2738d3e38c920683b50fd87455cc981ebd8
junehyung <jhcj910805@gmail.com>
2024-02-03 20:26:20 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ export function ReactLayout({ children }: any) {
   return (
     <div className={styles.root}>
       {children}
-      <span>{time}xd</span>
+      <span>{time}xcd</span>
     </div>
   );
 }
